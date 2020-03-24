152fd14a06f63b70c95885c3100b35bb37c82e5b
Gang Zhang <gzhangx@hotmail.com>
2020-03-23 23:03:47 -0400

    class

--- bluetank.ino
+++ bluetank.ino
@@ -5,8 +5,8 @@
  */
 
 #include "SoftwareSerial.h"
-const int BTBAUD = 57600;  //tank 115200, test 19200 //5 19200 //6 38400,  57600 7, 115200 8  AT+BAUD8
-const int SERBAUD = 115200; //tank 115200, test 19200
+const long BTBAUD = 57600;  //tank 115200, test 19200 //5 19200 //6 38400,  57600 7, 115200 8  AT+BAUD8
+const long SERBAUD = 115200; //tank 115200, test 19200
 const int BLUEINT = 3;    //tank 3, test 2
 const int FIREPIN = 9;
 int FIREPINTIMER = 0;
@@ -15,8 +15,43 @@ unsigned long lastFirePinCheck = millis();
 SoftwareSerial BTSerial(BLUEINT,4); // blue tx, blue rx
 
 const int BT_BUF_LEN=128;
-char sendstr[BT_BUF_LEN+16];
-int sendstrpos = 0; 
+
+class RecBuf{  
+    char receiveStr[BT_BUF_LEN+16];
+    int receivePos = 0;    
+    public:
+    String cmd = ""; 
+    String name = "";
+    String val = "";
+    bool onRecv(int c) {
+      if (receivePos < BT_BUF_LEN) {
+          receiveStr[receivePos++] = (char)c;
+          receiveStr[receivePos] = 0;
+          if (c == ':') {
+            receiveStr[receivePos-1]=0;
+            if (cmd == "") {
+              cmd = receiveStr;
+            } else  {
+              name = receiveStr; 
+            }
+            receivePos = 0;
+          }
+          if (c == '|' || c== '\n') {            
+            receiveStr[receivePos-1]=0;
+            if (receivePos >= 2 && receiveStr[receivePos-2] =='\r')receiveStr[receivePos-2]= 0;
+            receivePos = 0;
+            val = receiveStr;
+            //btCmdReceived(curBtCmd, curBtName, val);
+            return true;
+          }
+        }else {
+          receivePos = 0; //warning, over flow
+        }
+        return false;
+    }
+};
+
+RecBuf serBuf, blueBuf;
 unsigned long lastAvailableTime = millis();
 
 int motorCounter = 0;
@@ -29,7 +64,7 @@ int motorSpeed[]={0,0};
 
 
 void serprintln(String s) {
-  if (Serial) Serial.println(s);           
+  Serial.println(s);           
 }
 
 int pin9 = LOW;
@@ -88,12 +123,15 @@ void firePinCheck() {
   if (FIREPINTIMER <= 0) {
     lastFirePinCheck = millis();
     FIREPINTIMER = 0;
-    if (curFirePinState)    
+    if (curFirePinState) {
+      Serial.println("turning off fire");
       digitalWrite(FIREPIN, 0);
+    }
     curFirePinState = 0;
     return;
   }
   if (!curFirePinState) {
+    Serial.println("turning on fire");
      digitalWrite(FIREPIN, 1);
      curFirePinState = 1;
   }
@@ -102,7 +140,10 @@ void firePinCheck() {
   lastFirePinCheck = curtm;
 }
 
-void btCmdReceived(String cmd, String name, String val) {  
+void btCmdReceived(RecBuf *buf) {
+  String cmd = buf->cmd;
+  String name = buf->name;
+  String val = buf->val;
   Serial.println("gotxcmd " + cmd+ " val " + val);
   if (cmd == "l")  {
     motorSpeed[0] = val.toInt();
@@ -116,46 +157,22 @@ void btCmdReceived(String cmd, String name, String val) {
      FIREPINTIMER =  val.toInt();
   }
 }
-char receiveStr[BT_BUF_LEN+16];
-int receivePos = 0;
-String curBtCmd = "";
-String curBtName = "";
+
+
 void loop_bt() {  
-  while (BTSerial.available()){
+  if (BTSerial.available()){
         int c = BTSerial.read();
-        Serial.write(c);
-        if (receivePos < BT_BUF_LEN) {
-          receiveStr[receivePos++] = (char)c;
-          receiveStr[receivePos] = 0;
-          if (c == ':') {
-            receiveStr[receivePos-1]=0;
-            if (curBtCmd == "") {
-              curBtCmd = receiveStr;
-            } else  {
-              curBtName = receiveStr; 
-            }
-            receivePos = 0;
-          }
-          if (c == '|') {            
-            receiveStr[receivePos-1]=0;            
-            receivePos = 0;
-            String val = receiveStr;
-            btCmdReceived(curBtCmd, curBtName, val);
-            curBtCmd = "";
-          }
-        }else {
-          receivePos = 0; //warning, over flow
+        if (blueBuf.onRecv(c)) {
+          btCmdReceived(&blueBuf);
         }
-        //Serial.write(c);
   }
   // Keep reading from Arduino Serial Monitor and send to HC-05
-  while(Serial.available()){
+  if (Serial.available()){
     int c = Serial.read();
-    Serial.write(c);
-    BTSerial.write(c);      
-    if (c == '9') {
-      FIREPINTIMER = 1000;
-       
+    if (serBuf.onRecv(c)) {
+      btCmdReceived(&serBuf);
+      BTSerial.write(serBuf.val.c_str());
+      BTSerial.write("\r\n");
     }
   }
 }
